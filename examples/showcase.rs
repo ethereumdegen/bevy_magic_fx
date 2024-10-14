4aff58b864874c21de286b88f35774a932645a35
andy <admazzola@gmail.com>
2024-10-14 18:28:06 -0400

    heart works

--- examples/showcase.rs
+++ examples/showcase.rs
@@ -532,5 +532,15 @@ fn spawn_magic_fx(
                          ;
 
 
+       let  heart_fx = built_vfx_resource.magic_fx_variants.get("magic_fx_variants/heart.magicfx.ron").unwrap();
 
+          commands .spawn(SpatialBundle {
+
+                            transform: Transform::from_xyz(18.0,2.0,0.0) ,
+                            ..default()
+                        })
+                        .insert(MagicFxVariantComponent {
+                            magic_fx: heart_fx.clone(),
+                            start_time: time.elapsed(),
+                        }) ;          
 }
